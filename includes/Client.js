37ae3450d5c72218ee80864c20388765bf8d3ad4
Matheu Baille <fs2picsor@gmail.com>
2025-06-15 18:25:27 +0200

    AI implementation

--- includes/Client.js
+++ includes/Client.js
@@ -1,20 +1,37 @@
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
 const Database = require("../database/Database.js");
+// const { default: OpenAI } = require("openai");
+const { Mistral } = require("@mistralai/mistralai");
 
- 
+
 module.exports = class extends Client {
-    constructor () {
+    constructor() {
         super({
             intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildModeration, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
-			partials: [Partials.Channel, Partials.User, Partials.Message, Partials.GuildMember, Partials.ThreadMember]
-		});
+            partials: [Partials.Channel, Partials.User, Partials.Message, Partials.GuildMember, Partials.ThreadMember]
+        });
 
         this.db = new Database();
+        // this.openAIClient = this.loadOpenAIClient();
+        this.mistralClient = this.loadMistralClient();
         this.loadDatabase();
     }
 
     loadDatabase() {
         this.db.authenticate();
+    }
+
+    loadOpenAIClient() {
+        return new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY
+        })
+    }
+
+    loadMistralClient() {
+        // magistral-small-2506
+        return new Mistral({
+            apiKey: process.env.MISTRAL_API_KEY,
+        })
     }
 
 }
--- menus/PatchNoteTranslatePickLangSelectMenu.js
+++ menus/PatchNoteTranslatePickLangSelectMenu.js
@@ -25,9 +25,9 @@ class PatchNoteTranslatePickLangSelectMenu {
                     .setValue('fr'),
                 new StringSelectMenuOptionBuilder()
                     .setLabel(LocalisationManager.getString(
-                        'patchnote_lang_discord', lang, {'lang': lang}))
+                        'patchnote_lang_discord', lang, { 'lang': lang }))
                     .setDescription(LocalisationManager.getString(
-                        'patchnote_lang_discord_description', lang, {'lang': lang}))
+                        'patchnote_lang_discord_description', lang, { 'lang': lang }))
                     .setValue(`custom_${lang}`),
             )
             .setMinValues(1)
@@ -41,20 +41,22 @@ class PatchNoteTranslatePickLangSelectMenu {
     }
 
     static async onInteraction(interaction) {
-        await interaction.deferReply({flags: [MessageFlags.Ephemeral]});
+        await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
+
+        const { client } = interaction;
 
         const { PatchNoteComponent } = require("../components/PatchNoteComponent");
 
-        let selectedLang = interaction.values[0]; 
-        if(selectedLang.includes('custom_')) selectedLang = selectedLang.split('custom_')[1]
+        let selectedLang = interaction.values[0];
+        if (selectedLang.includes('custom_')) selectedLang = selectedLang.split('custom_')[1]
         const [prefix, patchnoteId] = interaction.customId.split('/');
         const lang = interaction.locale;
 
         const foundNodes = await PatchNoteNodes.findAll({
-            where: {patchnoteId: patchnoteId}
+            where: { patchnoteId: patchnoteId }
         });
 
-        if(!foundNodes || foundNodes.length === 0) {
+        if (!foundNodes || foundNodes.length === 0) {
             const container = PatchNoteNoNodesComponent.create(
                 lang, 'translate'
             );
@@ -64,12 +66,12 @@ class PatchNoteTranslatePickLangSelectMenu {
                 flags: [MessageFlags.IsComponentsV2]
             });
         }
-        
+
         if (selectedLang.includes("-")) selectedLang = selectedLang.split("-")[0];
 
         const options = { from: 'en', to: selectedLang };
 
-        const separator = "/////////////"
+        const separator = "[THISISASEPARATOR]"
         let mergedNodeStrings = "";
         foundNodes.forEach((node) => {
             mergedNodeStrings += `${node.content}${separator}`;
@@ -78,9 +80,43 @@ class PatchNoteTranslatePickLangSelectMenu {
         let translatedMergedNodeString;
         let translatedNodes;
 
+        // let openAIResponse;
+        let mistralResponse;
+
         try {
-            translatedMergedNodeString = await translate(mergedNodeStrings, options);
-            translatedNodes = translatedMergedNodeString.text.split(separator);
+            mistralResponse = await client.mistralClient.chat.complete({
+                model: "mistral-small-2503",
+                response_format: { type: "json_object" },
+                messages: [{
+                    role: 'system', content: `Translate each line. Return *only* valid json with this exact shape:
+                        {
+                            "translations": ["<translation-1>", "<translation-2>", …]
+                        }`
+                }, {
+                    role: 'user',
+                    content: `(from English to ${selectedLang}) ${mergedNodeStrings}`
+                }]
+            })
+
+            const raw = mistralResponse.choices[0].message.content;
+            let cleaned = raw.replaceAll(/[\n\r]/g, '');
+            cleaned = cleaned.replaceAll("```", "");
+            cleaned = cleaned.replaceAll("json", "");
+
+            translatedNodes = JSON.parse(cleaned);
+
+
+            // openAIResponse = await client.openAIClient.responses.create({
+            //     model: "gpt-3.5-turbo",
+            //     instructions: `You are a translator. Translate the following text to the specified language and return the translated text.\
+            //     Do not add any additional text or formatting. Each translation should be separated by the provided separator: ${separator}`,
+            //     input: `(from English to ${selectedLang}) ${mergedNodeStrings}`,
+            // });
+            // translatedNodes = openAIResponse.text.split(separator);
+
+
+            // translatedMergedNodeString = await translate(mergedNodeStrings, options);
+            // translatedNodes = translatedMergedNodeString.text.split(separator);
         } catch (e) {
             interaction.editReply(LocalisationManager.getString('translation_failed', lang))
             return;
