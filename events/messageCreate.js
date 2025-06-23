9b3e846930dfd3feb885a273eb88ae80e2783949
Lisa Stachlewitz <lisa.stachlewitz@gmail.com>
2025-06-23 22:47:04 +0200

    Changed formatting of ticket channel log messages

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,38 +1,47 @@
-const { Events, ChannelType, AuditLogEvent } = require('discord.js');
-const { LogUtils } = require('../utils/LogUtils');
+const { Events, PermissionsBitField } = require('discord.js');
+const Tickets = require('../database/models/Tickets');
+const { NoVariableResponseComponent } = require('../components/responses/NoVariableResponseComponent');
+const fs = require('fs').promises;
+const path = require('path'); 
+const { LocalisationManager } = require('../managers/LocalisationManager');
 
 module.exports = {
-    name: Events.MessageDelete,
+    name: Events.MessageCreate,
 
     async execute(message) {
         if (!message.guild) return;
 
-        try {
-            if(!message || !message.author || !message.author.username) {
-                return await LogUtils.sendLog(
-                    'log_message_deleted_no_data_found',
-                    message.guild,
-                    0xeb4034,
-                    {
-                        'messageChannelName': message.channel.name,
-                        'messageContent': message.content
-                    }
-                );
-            }
+        const { author } = message;
+        const { member } = message;
+        let msgContent = message.content;
+        const ticket = await Tickets.findOne({where: {channelId: message.channel.id}});
+        if(!ticket) return;
 
-            await LogUtils.sendLog(
-                'log_message_deleted',
-                message.guild,
-                0xeb4034,
-                {
-                    'messageChannelName': message.channel.name,
-                    'messageContent': message.content,
-                    'messageAuthor': message.author.username
-                }
-            );
+        const isAuthorTicketModerator = (ticket.moderator && ticket.moderator === author.id);
+        const isAuthorTicketCreator = (ticket.userId && ticket.userId === author.id);
 
-        } catch (error) {
-            console.error(`[channelDelete] Failed to send log message:`, error);
+        let authorRole = "User";
+
+        if (isAuthorTicketCreator) authorRole = "Author"
+        else if (isAuthorTicketModerator) authorRole = "Moderator"
+        else if (member.permissions.has(PermissionsBitField.Flags.ManageMessages)) 
+            authorRole = "Support"
+
+        if(message.components.length > 0 ) {
+            const firstComponent = message.components[0];
+            msgContent = "";
+            if(firstComponent.components.length > 0 && 
+                firstComponent.components[0].content
+            ) msgContent = firstComponent.components[0].content
+            
+            authorRole = "System";
         }
+
+        const { createdAt } = message;
+        const msgDateFormat = `[${createdAt.getDate()}/${createdAt.getMonth()}/${createdAt.getFullYear()} ${createdAt.getHours()}:${createdAt.getMinutes()}:${createdAt.getSeconds()}] (${author.id}) (${authorRole}) ${author.username} > ${msgContent}\n`
+
+        const logsFilePath = path.join(__dirname, `../files/tickets/${ticket.id}.txt`);
+        fs.appendFile(logsFilePath, msgDateFormat);
+
     },
 };
